1d400790a1eae0c3454c9b1863adcd88ae7a4bf9
Daniel Silveira <danielsilveira.contato@gmail.com>
2021-06-26 00:59:55 -0300

    NLW6(Aula 3): importando e executando recursos

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,8 @@
-const express = require('express');
-const route = express.Router()
+const express = require('express'); // importando express
+const route = express.Router() // Quem que é o route?
 
-route.get('/', (req, res) => res.render("index"))
+route.get('/', (req, res) => res.render("index")) // req = requisição | res = resposta => res.render("index(ejs)") <- irá renderizar o ejs
+// O '/' significa a página index(inicial)
+// ex: route.get('/sobre', (req, res) => res.render("sobre")) rota para página sobre
 
-module.exports = route
+module.exports = route // exportando a constante rout para o server.js
--- src/server.js
+++ src/server.js
@@ -1,7 +1,10 @@
 const express = require('express');
-const route = require('./routes')
-const server = express()
+const route = require('./routes') // Importando o routes
+const path = require('path') // Importando path que serve para informar diretórios
 
-server.set('view engine', 'ejs')
-server.use(route)
+const server = express() // Executando o express
+
+server.set('view engine', 'ejs') // set('view engine', 'ejs') view engine falando que o responsável é o ejs
+server.set('views', path.join(__dirname, 'views')) // Informando que caminho está a pasta 'views' - o join junta o src/ com views/ 
+server.use(route) // Usando o routes
 server.listen(3000, () => console.log("RODANDO"))
