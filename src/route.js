c9e9543c07c0a4ec488f39db85a5fe61e3659667
Daniel Silveira <danielsilveira.contato@gmail.com>
2021-07-03 15:29:52 -0300

    NLW6(Aula 4): DB and install sqlite

--- src/route.js
+++ src/route.js
@@ -9,7 +9,7 @@ route.get('/create-pass', (req, res) => res.render("index", {page: 'create-pass'
 
 route.get('/room/:roomid', (req, res) => res.render("room"))
 // Formato que o formulário de dentro da modal temm que passar a informação
-route.post('/room/:room/:question/:action', QuestionController.index) 
-route.post('/room/create-room', RoomController.create) 
+route.post('/question/:room/:question/:action', QuestionController.index) 
+route.post('/create-room', RoomController.create) 
 
 module.exports = route
